77002c37c32480e49d00c9bf8c06ec850850dd43
Geofrey Melvern <melverng23@gmail.com>
2026-04-15 20:21:26 +0700

    Refactor string output logic in soal4.cpp

--- soal4.cpp
+++ soal4.cpp
@@ -3,13 +3,13 @@ using namespace std;
 
 int main(){
     int n;
-    string hore = "Hore!";
+    string hore = "Hore!"; // buat string yang berisi setiap elemen yang ingin di cout
 
-    cin >> n;
+    cin >> n; // input n
 
-    for (int i = 0; i < hore.size(); i++){
-        if(hore[i] == 'H' || hore[i] == 'r') cout << hore[i];
-        else for (int j = 0; j < n; j++) cout << hore[i];
+    for (int i = 0; i < hore.size(); i++){ // looping setiap karakter "hore"
+        if(hore[i] == 'H' || hore[i] == 'r') cout << hore[i]; // untuk karakter 'H' dan 'r' cukup cetak sekali
+        else for (int j = 0; j < n; j++) cout << hore[i]; // untuk karakter 'o' dan 'e' cetak sebanyak n kali
     }
     return 0;
 }
