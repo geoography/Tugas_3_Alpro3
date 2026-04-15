81678ce7aa63adb56d149426559b00dca5a72fa5
Geofrey Melvern <melverng23@gmail.com>
2026-04-15 20:09:28 +0700

    Implement character repetition based on user input
    Add functionality to repeat characters based on input.

--- soal4.cpp
+++ soal4.cpp
@@ -1,1 +1,15 @@
+#include <iostream>
+using namespace std;
 
+int main(){
+    int n;
+    string hore = "Hore!";
+
+    cin >> n;
+
+    for (int i = 0; i < hore.size(); i++){
+        if(hore[i] == 'H' || hore[i] == 'r') cout << hore[i];
+        else for (int j = 0; j < n; j++) cout << hore[i];
+    }
+    return 0;
+}
