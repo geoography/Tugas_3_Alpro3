f84d9ba7e38ed1a3183e0a15d8ef94ccdb22fa4a
Geofrey Melvern <melverng23@gmail.com>
2026-04-15 20:23:30 +0700

    Refactor code comments and formatting in soal6.cpp

--- soal6.cpp
+++ soal6.cpp
@@ -5,12 +5,12 @@ int main(){
     int n;
     long long total = 0;
     
-    cin >> n;
+    cin >> n; // input n
 
-    for (int i = 2; i <= n; i += 2){
-        total += i*i;
+    for (int i = 2; i <= n; i += 2){ // loop mulai dari 2 hingga bilangan genap yang lebih kecil sama dengan n
+        total += i*i; // variabel total merupakan penjumlahan setiap kuadrat dari semua elemen genap
     }
 
-    cout << total << endl;
+    cout << total << endl; // cout nilai total
     return 0;
 }
