40111f8af4da3289d214aea1ab7030283fcebdde
Geofrey Melvern <melverng23@gmail.com>
2026-04-15 20:09:53 +0700

    Implement sum of squares for even numbers
    Calculate the sum of squares of even numbers up to n.

--- soal6.cpp
+++ soal6.cpp
@@ -1,1 +1,16 @@
+#include <iostream>
+using namespace std;
 
+int main(){
+    int n;
+    long long total = 0;
+    
+    cin >> n;
+
+    for (int i = 2; i <= n; i += 2){
+        total += i*i;
+    }
+
+    cout << total << endl;
+    return 0;
+}
