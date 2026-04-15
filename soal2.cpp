accedff9de2033248465c49faccc5bea10c1c5d7
Geofrey Melvern <melverng23@gmail.com>
2026-04-15 20:09:07 +0700

    Implement character removal from input string
    Added functionality to remove specified character from input string and count occurrences.

--- soal2.cpp
+++ soal2.cpp
@@ -1,1 +1,26 @@
+#include <iostream>
+#include <cctype>
+#include <cstdlib>
+// #include <string>
+using namespace std;
 
+int main(){
+    int cnt = 0;
+    string input;
+    char target;
+
+    getline(cin, input);
+    cin >> target;
+
+    system("cls");
+
+    for (int i = 0; i < input.size(); i++){
+        if (target != tolower(input[i])){
+            cnt ++;
+            cout << input[i];  
+        } 
+    }
+
+    cout << "\nKarakter terhapus: " << input.size() - cnt << endl;
+    return 0;
+}
