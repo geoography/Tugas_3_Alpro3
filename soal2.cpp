1520227d3941bffd952fc5b3c16c253b67a1a4e0
Geofrey Melvern <melverng23@gmail.com>
2026-04-15 20:19:33 +0700

    Enhance input handling and comments in soal2.cpp
    Updated input handling to allow multiple words and improved comments.

--- soal2.cpp
+++ soal2.cpp
@@ -9,18 +9,19 @@ int main(){
     string input;
     char target;
 
-    getline(cin, input);
-    cin >> target;
+    getline(cin, input); // pakai getline supaya bisa input lebih dari satu kata
+    cin >> target; // input huruf target 
 
     system("cls");
 
     for (int i = 0; i < input.size(); i++){
-        if (target != tolower(input[i])){
-            cnt ++;
-            cout << input[i];  
+        // cek tiap karakter yang ada di string
+        if (target != tolower(input[i])){  // jika tidak sama dengan karakter target
+            cnt ++; // hitung banyak karakter yang tidak sama dengan karakter target
+            cout << input[i];  // cout karakter yang tidak sama dengan karakter target
         } 
     }
 
-    cout << "\nKarakter terhapus: " << input.size() - cnt << endl;
+    cout << "\nKarakter terhapus: " << input.size() - cnt << endl; // hitung karakter yang memiliki nilai sama dengan target
     return 0;
 }
