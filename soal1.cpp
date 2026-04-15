7bcc67278276fcf8f4208cc8a000649b7fcce194
Geofrey Melvern <melverng23@gmail.com>
2026-04-15 20:14:54 +0700

    Add sequence generation in soal1.cpp
    Implement a program that generates a sequence based on alternating increments of 3 and 5.

--- soal1.cpp
+++ soal1.cpp
@@ -1,1 +1,24 @@
+#include <iostream>
+using namespace std;
 
+int main() {
+    int n;
+    cin >> n; // input jumlah suku
+
+    int angka = 1; // suku pertama
+    cout << angka << " ";
+
+    for(int i = 2; i <= n; i++) {
+        // pola selang-seling:
+        // urutan genap → +5
+        // urutan ganjil → +3
+        if(i % 2 == 0)
+            angka += 5;
+        else
+            angka += 3;
+
+        cout << angka << " ";
+    }
+
+    return 0;
+}
